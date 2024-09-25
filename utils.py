e664e7a763e967a0bce93a9b3901b1933535b14a
Avinash Mall <111482444+avinash-mall@users.noreply.github.com>
2024-09-25 14:45:48 +0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,6 +4,8 @@ import re
 import unicodedata
 import warnings
 from datetime import datetime
+from urllib.parse import urlencode
+
 from elasticsearch import Elasticsearch, exceptions, ElasticsearchWarning
 import fitz  # PyMuPDF
 import docx
@@ -18,6 +20,269 @@ from streamlit_autorefresh import st_autorefresh
 from tokenizers import Tokenizer
 from semantic_text_splitter import TextSplitter
 import streamlit as st
+import yaml
+import streamlit_authenticator as stauth
+from yaml.loader import SafeLoader
+import smtplib
+from email.message import EmailMessage
+import secrets
+from urllib.parse import parse_qs
+
+def send_email(subject, body, to_email, config):
+    msg = EmailMessage()
+    msg.set_content(body)
+    msg['Subject'] = subject
+    msg['From'] = config['smtp']['username']
+    msg['To'] = to_email
+
+    with smtplib.SMTP(config['smtp']['server'], config['smtp']['port']) as server:
+        #if config['smtp']['use_tls']:
+        #    server.starttls()
+        #server.login(config['smtp']['username'], config['smtp']['password'])
+        server.send_message(msg)
+
+
+def send_reset_password_email(name, new_password, to_email, config):
+    subject = "Your New Password"
+    body = f"Hey {name},\n\nHere is your new password:\n\n {new_password}\n\nPlease change it once you log in."
+
+    send_email(subject, body, to_email, config)
+
+
+def send_forgot_username_email(name, username, to_email, config):
+    subject = "Your Username Reminder"
+    body = f"Hey {name},\n\nYour username is: \n\n{username}\n\n"
+
+    send_email(subject, body, to_email, config)
+
+# Helper to determine if password is alredy hashed
+def is_bcrypt_hash(s):
+    return s.startswith(('$2a$', '$2b$', '$2x$', '$2y$')) and len(s) == 60
+
+
+# Hash new plaintext passwords only
+def hash_plaintext_passwords(config):
+    plaintext_passwords = {}
+    for user, details in config['credentials']['usernames'].items():
+        # Check if the password is not a bcrypt hash
+        if not is_bcrypt_hash(details['password']):
+            plaintext_passwords[user] = details['password']
+
+    if plaintext_passwords:
+        hashed_passwords = stauth.Hasher(list(plaintext_passwords.values())).generate()
+        for user, hashed_pw in zip(plaintext_passwords.keys(), hashed_passwords):
+            config['credentials']['usernames'][user]['password'] = hashed_pw
+
+    return config
+
+# Load config.yaml
+def load_auth_config():
+    with open('config.yaml') as file:
+        config = yaml.load(file, Loader=SafeLoader)
+    return config
+
+
+# Update config.yaml after any changes (e.g., registration or password reset)
+def update_yaml(config):
+    with open('config.yaml', 'w') as file:
+        yaml.dump(config, file, default_flow_style=False)
+
+
+# Authentication setup
+def setup_authenticator():
+    config = load_auth_config()
+    authenticator = stauth.Authenticate(
+        config['credentials'],
+        config['cookie']['name'],
+        config['cookie']['key'],
+        config['cookie']['expiry_days'],
+        config['pre-authorized']
+    )
+    return authenticator, config
+
+
+# Login function
+def login(authenticator):
+    custom_fields = {
+        'Form name': 'Please Login',
+        'Username': 'Enter Username',
+        'Password': 'Enter Password',
+        'Login': 'Sign In'
+    }
+
+    name, authentication_status, username = authenticator.login(
+        location='main',
+        max_concurrent_users=5,
+        max_login_attempts=3,
+        fields=custom_fields,
+        captcha=False,
+        clear_on_submit=True,
+        key='LoginWidget'
+    )
+
+    return name, authentication_status, username
+
+
+# Logout function
+def logout(authenticator):
+    try:
+        authenticator.logout('Logout', location='main', key='LogoutWidget')
+        #st.toast('User logged out successfully', icon="✅")
+    except Exception as e:
+        st.toast(e, icon="⚠️")
+
+# Generate Token
+def generate_verification_token():
+    # Generate a unique token (e.g., use the secrets module)
+    return secrets.token_urlsafe(16)
+
+
+# Register function
+# def register_user(authenticator, config):
+#     try:
+#         email_of_registered_user, username_of_registered_user, name_of_registered_user = authenticator.register_user(
+#             location='main',
+#             pre_authorization=False,
+#             clear_on_submit=True,
+#             key='RegisterWidget'
+#         )
+#         if email_of_registered_user:
+#             st.toast('User registered successfully', icon="✅")
+#             update_yaml(config)
+#     except Exception as e:
+#         st.toast(e, icon="⚠️")
+
+
+# Temporary storage for unverified users (can be a dictionary or external storage)
+unverified_users = {}
+
+
+def register_user(authenticator, config):
+    try:
+        email_of_registered_user, username_of_registered_user, name_of_registered_user = authenticator.register_user(
+            location='main',
+            pre_authorization=False,
+            clear_on_submit=True,
+            key='RegisterWidget'
+        )
+        if email_of_registered_user:
+            st.toast('User registration initiated. Please check your email to verify.', icon="✅")
+            # Generate a verification token
+            verification_token = generate_verification_token()
+            # Store the user temporarily with the token
+            unverified_users[email_of_registered_user] = {
+                'username': username_of_registered_user,
+                'name': name_of_registered_user,
+                'password': config['credentials']['usernames'][username_of_registered_user]['password'],
+                # Assuming password is hashed
+                'verification_token': verification_token
+            }
+
+            # Load the domain from config.yaml
+            app_domain = config['app']['domain']
+
+            # URL encode the parameters (token and email)
+            query_params = urlencode({
+                'token': verification_token,
+                'email': email_of_registered_user
+            })
+
+            # Create the verification link, now including the page parameter
+            verification_link = f"{app_domain}?&{query_params}"
+
+            # Send verification email
+            subject = "Email Verification"
+            body = f"Hello {name_of_registered_user},\n\nPlease verify your email by clicking the link below:\n{verification_link}"
+            send_email(subject, body, email_of_registered_user, config)
+
+    except Exception as e:
+        st.toast(e, icon="⚠️")
+
+
+# This is the verification handler
+def verify_email(token, email, config):
+    #print(unverified_users)
+    for user_email, user_data in unverified_users.items():
+        if user_email == email and user_data['verification_token'] == token:
+            # Move user from unverified to verified list (add to config.yaml)
+            config['credentials']['usernames'][user_data['username']] = {
+                'email': user_email,
+                'name': user_data['name'],
+                'password': user_data['password']  # Assuming password is already hashed
+            }
+            # Update config.yaml
+            update_yaml(config)
+            st.toast('User Verified Successfully.', icon="✅")
+            # Remove user from unverified users
+            del unverified_users[user_email]
+            return True
+        else:
+            return False
+
+
+# Streamlit function to check URL parameters
+def verify_page(config=load_auth_config()):
+    if st.query_params.token or st.query_params.email:
+        token = st.query_params.token  # Extract token
+        email = st.query_params.email  # Extract email
+        print(token, email)
+        # Call the verification function
+        if verify_email(token, email, config):
+            st.success("Your email has been verified successfully. You can now log in.")
+        else:
+            st.error("Invalid or expired verification link.")
+    else:
+        st.error("Invalid request. Token or email is missing.")
+
+
+# Reset password function
+def reset_password(authenticator, username, config):
+    try:
+        if authenticator.reset_password(username, location='main', clear_on_submit=True, key='ResetPasswdWidget'):
+            st.toast('Password modified successfully', icon="✅")
+            update_yaml(config)
+    except Exception as e:
+        st.toast(e, icon="⚠️")
+
+
+# Forgot password function
+def forgot_password(authenticator, config):
+    try:
+        username_of_forgotten_password, email_of_forgotten_password, new_random_password = authenticator.forgot_password(location='main', captcha=True, key='ForgotPasswdWidget')
+        if username_of_forgotten_password:
+            user_name = config['credentials']['usernames'][username_of_forgotten_password][
+                'name']  # Assuming you store the name in the config
+            update_yaml(config)
+            send_reset_password_email(user_name, new_random_password, email_of_forgotten_password, config)
+            st.toast('New password sent securely', icon="✅")
+        elif username_of_forgotten_password == False:
+            st.toast('Username not found', icon="⚠️")
+    except Exception as e:
+        st.toast(e, icon="⚠️")
+
+# Forgot username function
+def forgot_username(authenticator, config):
+    try:
+        username_of_forgotten_username, email_of_forgotten_username = authenticator.forgot_username(location='main', captcha=True, key='ForgotUserWidget')
+        if username_of_forgotten_username:
+            user_name = config['credentials']['usernames'][username_of_forgotten_username]['name']
+            update_yaml(config)
+            send_forgot_username_email(user_name, username_of_forgotten_username, email_of_forgotten_username, config)
+            st.toast('Username sent securely', icon="✅")
+        elif username_of_forgotten_username == False:
+            st.toast('Email not found', icon="⚠️")
+    except Exception as e:
+        st.toast(e, icon="⚠️")
+
+# Forgot username function
+def update_user(authenticator, config):
+    try:
+        if authenticator.update_user_details(st.session_state['username'], location='main', clear_on_submit=True, key='UpdateUserWidget'):
+            st.toast('Entries updated successfully', icon="✅")
+            update_yaml(config)
+    except Exception as e:
+        st.toast(e, icon="⚠️")
+
 
 # Load environment variables from .env file
 load_dotenv()
@@ -133,7 +398,7 @@ def refresh_metrics(display_metrics):
         mem_container.metric(label="Memory Usage", value=f"{mem_usage}%")
         health_container.metric(label="Model Health", value=model_health)
 
-def append_date_time_to_prompt(prompt_content, append_date_time):
+def append_date_time_to_prompt(prompt_content, append_date_time, name):
     """Append date and time information to the selected prompt content if enabled."""
     if prompt_content is None:
         prompt_content = ""  # Initialize as an empty string if None
@@ -145,7 +410,7 @@ def append_date_time_to_prompt(prompt_content, append_date_time):
         current_day = now.strftime("%A")
         current_timezone = now.tzname()
 
-        date_time_info = f"The current time is: {current_time}, date: {current_date}, day: {current_day}, timezone: {current_timezone}."
+        date_time_info = f"The current time is: {current_time}, date: {current_date}, day: {current_day}, timezone: {current_timezone}, Username: {name}"
         prompt_content += "\n" + date_time_info
 
     return prompt_content
@@ -190,13 +455,13 @@ def extract_text_from_pdf(uploaded_file, use_ocr=False, languages=None):
         return text
 
     except RuntimeError as e:
-        st.error("Failed to read the PDF file. Please ensure the file is a valid PDF.")
-        st.error(f"Error details: {str(e)}")
+        st.toast("Failed to read the PDF file. Please ensure the file is a valid PDF.", icon="⚠️")
+        st.toast(f"Error details: {str(e)}", icon="⚠️")
         return None
 
     except Exception as e:
-        st.error("An unexpected error occurred while processing the PDF file.")
-        st.error(f"Error details: {str(e)}")
+        st.toast("An unexpected error occurred while processing the PDF file.", icon="⚠️")
+        st.toast(f"Error details: {str(e)}", icon="⚠️")
         return None
 
 
@@ -209,7 +474,7 @@ def extract_text_from_image(uploaded_file, languages=None):
         text = "\n".join(result)
         return text
     else:
-        st.warning("Please select at least one language for OCR.")
+        st.toast("Please select at least one language for OCR.", icon="⚠️")
         return None
 
 def extract_text_from_file(uploaded_file, ocr_option=None, languages=None):
@@ -225,7 +490,7 @@ def extract_text_from_file(uploaded_file, ocr_option=None, languages=None):
         doc = docx.Document(uploaded_file)
         return "\n".join([para.text for para in doc.paragraphs])
     else:
-        st.error("Unsupported file type.")
+        st.toast("Unsupported file type.", icon="⚠️")
         return None
 
 def search_elasticsearch(query, index_name):
@@ -246,11 +511,11 @@ def search_elasticsearch(query, index_name):
         hits = response['hits']['hits']
         return hits
     except exceptions.RequestError as e:
-        st.error(f"Request Error: {e.info}")
+        st.toast(f"Request Error: {e.info}", icon="⚠️")
     except exceptions.ConnectionError as e:
-        st.error(f"Connection Error: {e.errors}")
+        st.toast(f"Connection Error: {e.errors}", icon="⚠️")
     except Exception as e:
-        st.error(f"General Error: {str(e)}")
+        st.toast(f"General Error: {str(e)}", icon="⚠️")
 
 
 def display_debug_info(summary, prompt, messages):
@@ -284,3 +549,55 @@ def clear_chat_history():
         st.session_state["messages"] = []
     if "system_instruction" in st.session_state:
         st.session_state["system_instruction"] = None
+
+def setup_authentication_menu():
+    # Setup Authenticator
+    authenticator, config = setup_authenticator()
+
+    if 'hashed_done' not in st.session_state:
+        config = hash_plaintext_passwords(config)
+        update_yaml(config)
+        st.session_state.hashed_done = True
+
+    col1, col2, col3, col4 = st.columns(4)
+
+    # Login
+    with col1:
+        with st.popover("Login"):
+            name, authentication_status, username = login(authenticator)
+
+    # Handle Authentication Logic
+    if st.session_state["authentication_status"]:
+        st.markdown(f':thumbsup: Welcome *{name}*')
+        with col2:
+            # If the user is authenticated
+            logout(authenticator)
+        with col3:
+            # Reset password for authenticated users
+            with st.popover("Reset Password"):
+                reset_password(authenticator, username, config)
+        with col4:
+            # Update details for authenticated users
+            with st.popover("Update User Details"):
+                update_user(authenticator, config)
+        return name
+    elif st.session_state["authentication_status"] == False:
+        st.toast('Incorrect Username or Password', icon="⚠️")
+        with col2:
+            with st.popover("Register"):
+                register_user(authenticator, config)
+        with col3:
+            with st.popover("Forgot Password"):
+                forgot_password(authenticator, config)
+    elif st.session_state["authentication_status"] == None:
+        st.toast('Please enter your username and password', icon="⚠️")
+        with col2:
+            # Handle Registration
+            with st.popover("Register"):
+                register_user(authenticator, config)
+        with col3:
+            with st.popover("Forgot Password"):
+                forgot_password(authenticator, config)
+        with col4:
+            with st.popover("Forgot Username"):
+                forgot_username(authenticator, config)
