93e8eaccac70f46d0c7a644cefa44a4c96eed745
Avinash Mall <111482444+avinash-mall@users.noreply.github.com>
2024-09-26 11:27:03 +0400

    Update manage-documents.py

--- pages/manage-documents.py
+++ pages/manage-documents.py
@@ -18,14 +18,18 @@ from utils import (
     refresh_metrics,
     display_advanced_settings,
     toggle_display_metrics,
-    setup_authentication_menu
+    setup_authentication_menu,
+    create_index,
+    delete_index,
+    list_documents,
+    index_text
 )
-
 # Suppress Elasticsearch system indices warnings
 warnings.filterwarnings("ignore", category=ElasticsearchWarning)
 
 # Load environment variables from .env file
 load_dotenv()
+ADMIN_EMAIL = os.getenv('ADMIN_EMAIL')
 
 # Initialize Elasticsearch with authentication
 es = Elasticsearch(
@@ -40,9 +44,12 @@ st.set_page_config(page_icon="🔧", layout="wide")
 st.title("🔧 Document Management")
 
 # Setup Authenticator
-name = setup_authentication_menu()
+name, config = setup_authentication_menu()
 
 if st.session_state["authentication_status"]:
+    user_email = config['credentials']['usernames'][st.session_state["username"]]['email']
+    user_index = f"user_{user_email.replace('@', '_').replace('.', '_')}"
+
     # Sidebar settings
     st.sidebar.title("Admin Settings")
 
@@ -56,104 +63,12 @@ if st.session_state["authentication_status"]:
     # Dropdown for index management actions
     index_action = st.sidebar.selectbox("Select Action", ["Create Index", "Delete Index", "List Documents"])
 
-    def generate_unique_id(text):
-        hash_object = hashlib.sha256()
-        hash_object.update(text.encode('utf-8'))
-        unique_id = hash_object.hexdigest()
-        return unique_id
-
-    def index_text(index_name, text, document_name, total_files, file_number):
-        clean_text_content = clean_text(text)
-        local_time = datetime.now()
-        utc_time = local_time.astimezone(pytz.utc)
-        timestamp = utc_time.isoformat()
-        chunks = split_text_semantically(clean_text_content)
-        total_chunks = len(chunks)
-        progress_text = f"Indexing document {file_number}/{total_files}. Please wait..."
-        my_bar = st.progress(0, text=progress_text)
-
-        for i, chunk in enumerate(chunks):
-            if chunk:
-                doc_id = generate_unique_id(chunk)
-                embedding = model.encode(chunk).tolist()
-                body = {
-                    "text": chunk,
-                    "embedding": embedding,
-                    "document_name": document_name,
-                    "timestamp": timestamp
-                }
-                es.index(index=index_name, id=doc_id, body=body)
-                my_bar.progress((i + 1) / total_chunks, text=progress_text)
-
-        my_bar.empty()
-
-    def list_documents(index_name):
-        query = {
-            "size": 10000,
-            "_source": ["document_name", "timestamp"],
-            "query": {
-                "match_all": {}
-            }
-        }
-        response = es.search(index=index_name, body=query)
-
-        document_data = {}
-        for hit in response["hits"]["hits"]:
-            source = hit["_source"]
-            doc_name = source.get("document_name", "Unknown Document")
-            timestamp = source.get("timestamp", "No Timestamp")
-
-            if doc_name in document_data:
-                document_data[doc_name]["number_of_chunks"] += 1
-            else:
-                document_data[doc_name] = {
-                    "document_name": doc_name,
-                    "number_of_chunks": 1,
-                    "timestamp": timestamp
-                }
-
-        if not document_data:
-            return pd.DataFrame(columns=["document_name", "number_of_chunks", "date_time_added"])
-
-        document_df = pd.DataFrame(document_data.values())
-        document_df["timestamp"] = pd.to_datetime(document_df["timestamp"], errors='coerce').dt.strftime('%Y-%m-%d %H:%M:%S')
-        document_df = document_df.rename(columns={"timestamp": "date_time_added"})
-
-        return document_df
-
-    def create_index(index_name):
-        field_name = "embedding"
-        similarity_type = os.getenv("SIMILARITY_TYPE", "cosine")
-        default_dims = int(os.getenv("DEFAULT_DIMS", 1024))
-
-        if es.indices.exists(index=index_name):
-            st.error("Index already exists")
-            return
-
-        mappings = {
-            "properties": {
-                field_name: {
-                    "type": "dense_vector",
-                    "dims": default_dims,
-                    "index": "true",
-                    "similarity": similarity_type,
-                }
-            }
-        }
-
-        es.indices.create(index=index_name, body={"mappings": mappings})
-        st.success(f"Index '{index_name}' with {default_dims} dimensions and similarity '{similarity_type}' created successfully")
-
-    def delete_index(index_name):
-        if not es.indices.exists(index=index_name):
-            st.error("Index not found")
-            return
-        es.indices.delete(index=index_name)
-        st.success(f"Index '{index_name}' deleted successfully")
-
     if index_action == "Create Index":
         st.sidebar.subheader("Create a New Index")
-        new_index_name = st.sidebar.text_input("New Index Name")
+        if user_email == ADMIN_EMAIL:
+            new_index_name = st.sidebar.text_input("New Index Name")
+        else:
+            new_index_name = user_index
 
         if st.sidebar.button("Create Index"):
             if not new_index_name.strip():
@@ -166,15 +81,22 @@ if st.session_state["authentication_status"]:
 
     elif index_action == "Delete Index":
         st.sidebar.subheader("Delete an Index")
-        indexes = [index for index in es.indices.get_alias(index="*").keys() if not index.startswith('.')]
-        selected_index_name = st.sidebar.selectbox("Select Index to Delete", options=indexes)
+        if user_email == ADMIN_EMAIL:
+            indexes = [index for index in es.indices.get_alias(index="*").keys() if not index.startswith('.')]
+            selected_index_name = st.sidebar.selectbox("Select Index to Delete", options=indexes)
+        else:
+            selected_index_name = user_index
         if st.sidebar.button("Delete Index"):
             delete_index(selected_index_name)
 
     elif index_action == "List Documents":
         st.sidebar.subheader("List Documents in an Index")
-        indexes = [index for index in es.indices.get_alias(index="*").keys() if not index.startswith('.')]
-        selected_index_name = st.sidebar.selectbox("Select Index to List Documents", options=indexes)
+        if user_email == ADMIN_EMAIL:
+            indexes = [index for index in es.indices.get_alias(index="*").keys() if not index.startswith('.')]
+            selected_index_name = st.sidebar.selectbox("Select Index to List Documents", options=indexes)
+        else:
+            selected_index_name = user_index
+
         if st.sidebar.button("List Documents"):
             document_df = list_documents(selected_index_name)
             if not document_df.empty:
@@ -186,9 +108,11 @@ if st.session_state["authentication_status"]:
     uploaded_files = st.file_uploader("Upload text, PDF, or Word documents", type=["txt", "pdf", "docx"], accept_multiple_files=True)
 
     if uploaded_files:
-        indexes = [index for index in es.indices.get_alias(index="*").keys() if not index.startswith('.')]
-        index_for_upload = st.selectbox("Select Index to Upload Document", options=indexes)
-
+        if user_email == ADMIN_EMAIL:
+            indexes = [index for index in es.indices.get_alias(index="*").keys() if not index.startswith('.')]
+            index_for_upload = st.selectbox("Select Index to Upload Document", options=indexes)
+        else:
+            index_for_upload = user_index
         extraction_method = st.selectbox("Choose extraction method for all PDFs", ["Read Normal", "Read Using PDF OCR"])
 
         if extraction_method == "Read Using PDF OCR":
